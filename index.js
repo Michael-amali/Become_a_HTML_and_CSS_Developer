2912bc9a7927f32dc814b0ce06c9da5ff8a53494
Michael-amali <michael.acheampon@amalitech.org>
2021-04-15 16:41:36 +0100

    implemented the javascript aspect of the coding

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
-const info= {
-    input_1_1: "invalid first name",
-    input_1_2: "invalid last name",
-    input_1_3: "invalid email",
-    input_1_4: "invalid phone number",
-    input_1_5: "invalid website",
-    input_1_6: "invalid company",
+// const info= {
+//     input_1_1: "invalid first name",
+//     input_1_2: "invalid last name",
+//     input_1_3: "invalid email",
+//     input_1_4: "invalid phone number",
+//     input_1_5: "invalid website",
+//     input_1_6: "invalid company",
 
-}
+// }
 const userInputs = document.querySelectorAll('.input-flip');
 const userInputArray = [];
 
-function submitButtonHandler(){
+
+// disabling all fields until every field is filled
+var form = document.querySelector('form');
+var inputs = document.querySelectorAll('.input-flip');
+var required_inputs = document.querySelectorAll('.input-flip');
+var register = document.querySelector('input[type="submit"]');
+form.addEventListener('keyup', function(e) {
+ var disabled = false
+ inputs.forEach(function(input, index) {
+ if (input.value === '' || !input.value.replace(/\s/g, '').length) {
+ disabled = true
+ }
+ })
+ if (disabled) {
+ register.setAttribute('disabled', 'disabled');
+ } else {
+ register.removeAttribute('disabled');
+ }
+})
+
+const numTotal = document.querySelector('#input_1_4').value.toString();
+console.log(numTotal);
+
+function submitButtonHandler(){ 
     for(const input of userInputs){
-        //checks if empty, then use the id of that element to access an object having id and corresponding msg
-        if(input.value === ""){
-            userInputArray.push(info[input.id]);
-        }
-        else{
-            userInputArray.push(input.value);
-        }    
+        userInputArray.push(input.value);  
     }
-    alert(userInputArray);
+    // none of the items must be empty in order for the alert() to run
+    if(!userInputArray.includes("") && numTotal == 10){
+        alert(userInputArray);
+    }     
+
 }
 
 const submitButton = document.querySelector('#input_1_8');
